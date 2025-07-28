b74869883940987c53a7e2ae700e8c028ae8729b
Busola Falope <123890450+Rosietta-UIUX@users.noreply.github.com>
2025-07-28 17:05:14 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true
+  }
+})
+
+
+
+const nextConfig = {
+    images: {
+    domains: [
+      'api.aquatrackinc.com'
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
